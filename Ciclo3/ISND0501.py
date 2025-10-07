2101085c783b4f16008cdf48c458e57abb7d805b
BlaztNinja <joselvelasquez005@gmail.com>
2025-10-07 16:57:34 -0600

    trabajo

---
+++ Ciclo3/ISND0501.py
@@ -0,0 +1,8 @@
+nombre = "Alvin"
+
+print(len(nombre)) #prints length starting from 0
+
+print(nombre[2]) #nombre is an array, looking for the 3rd letter of the thingy
+
+print(nombre[2:5]) #print letter 3 to 6
+
