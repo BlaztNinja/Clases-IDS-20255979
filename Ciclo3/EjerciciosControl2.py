713f40edbfb3f685a4126be1c94ddc70f6871ad1
BlaztNinja <joselvelasquez005@gmail.com>
2025-10-09 17:53:37 -0600

    wesdfzd

---
+++ Ciclo3/EjerciciosControl2.py
@@ -0,0 +1,49 @@
+#ejercicio 1
+"""
+costE = 1.25
+costF = 1.30
+costM = 1.14
+
+enero = int(input("Cantidad enero: "))
+
+febrero = int(input("Cantidad febrero: "))
+
+marzo = int(input("Cantidad marzo5: "))
+
+print("El costo total es:", costE*enero+costF*febrero+costM*marzo)
+"""
+
+#ejercicio 2
+"""
+dias = ["lunes","martes","miercoles","jueves","viernes","sabado","domingo"]
+lu = int(input("lunes:"))
+dias[0] = lu # type: ignore
+ma = int(input("martes:"))
+dias[1] = ma # type: ignore
+mie = int(input("miercoles:"))
+dias[2] = mie # type: ignore
+jue = int(input("jueves:"))
+dias[3] = jue # type: ignore
+vie = int(input("viernes:"))
+dias[4] = vie # type: ignore
+sab = int(input("sabado:"))
+dias[5] = sab # type: ignore
+dom = int(input("domingo:"))
+dias[6] = dom # type: ignore
+print(dias)
+"""
+
+#ejercicio 3
+"""
+alumnos = ("Diego","Frank","Calvo","Abby","Alvin","Medranito","Alan")
+niñio = int(input("ingreses el orden del niño que desea saber (1-7): "))
+print(f"El alumno que ingreso como numero {niñio} es {alumnos[niñio-1]}") 
+"""
+
+#ejercicio 4
+"""
+nombre = input("nombre:")
+apellido = input("apellido:")
+
+print(f"{nombre}{apellido}@ISND.com")
+"""
